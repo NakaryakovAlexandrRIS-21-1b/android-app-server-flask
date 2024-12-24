292357b2beed9ef6f05dce4c5bd69587bb824081
alex <sasha080603@gmail.com>
2024-12-25 01:51:18 +0500

    note initialization

--- server.py
+++ server.py
@@ -10,3 +10,10 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024  # 1 MB
 # Настройка логгера
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+class Note:
+    def __init__(self, note_id, deadline, text=""):
+        self.note_id = note_id
+        self.deadline = deadline
+        self.text = text
+        self.timer = None
